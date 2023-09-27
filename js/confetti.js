a1c24036ca439dc46c9a0f324378e207e2ec0739
oguzfarsak <oguz@apps.com.tr>
2023-09-28 00:17:42 +0300

    confetti js removed

--- js/confetti.js
+++
@@ -1,8 +0,0 @@
-import confetti from 'https://cdn.skypack.dev/canvas-confetti';
-
-const btn = document.getElementById("btn")
-function makeConfetti(){
-    confetti()
-}
-
-btn.addEventListener("click", makeConfetti)
